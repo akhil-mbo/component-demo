bb1be99772532e0d4244db0c7c596064818c6e1f
Akhil Aravindakshan <aaravindakshan@mbopartners.com>
2022-02-09 15:51:57 +0530

    Icon Components

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Icon from "../../utils/Constants/Icon";
+
+import ThreeDots from "./ThreeDots";
+
+interface IconProps {
+  name: string;
+}
+
+const Icons: React.FC<IconProps> = ({ name }) => {
+  switch (name) {
+    case Icon.THREE_DOTS:
+      return <ThreeDots />;
+
+    default:
+      return <div>No icon found</div>;
+  }
+};
+
+export default Icons;
